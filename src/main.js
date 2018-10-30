e3bb3fb4b83dacc36de6d4dffd3da26b4390a0c5
Gustavo N <gustavostnt@gmail.com>
2018-10-29 23:48:53 -0300

    finished?

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 
-const vigencia = (facts, schema) => {
+const App = {};
+
+App.main = (data, filterFn) => {
+    const facts = document.querySelector("#facts");
+    const schema = document.querySelector("#schema");
+    const result = document.querySelector("#result");
+    const filter = document.querySelector("#filter");
+    
+    facts.value = JSON.stringify(data.facts);
+    schema.value = JSON.stringify(data.schema);
+
+    filter.addEventListener("click", () => {
+        const newFacts = JSON.parse(facts.value);
+        const newSchema = JSON.parse(schema.value);
+        const newResult = filterFn(newFacts, newSchema);
+        console.log(newResult);
+        result.value = JSON.stringify(newResult);
+    });
+};
+
+App.filterFn = (facts, schema) => {
     const trueFacts = facts.filter(f => f[3]);
     const oneToOneAttributes = schema.filter(s => s[2] === "one");
     const isOneToOne = (curr) => {
@@ -16,9 +36,9 @@ const vigencia = (facts, schema) => {
     return latest.reverse();
 };
 
-const info = {};
+App.data = {};
 
-info.facts = [
+App.data.facts = [
   ['gabriel', 'endereço', 'av rio branco, 109', true],
   ['joão', 'endereço', 'rua alice, 10', true],
   ['joão', 'endereço', 'rua bob, 88', true],
@@ -29,17 +49,18 @@ info.facts = [
   ['gabriel', 'telefone', '56789-1010', true],
 ];
 
-info.schema = [
+App.data.schema = [
     ['endereço', 'cardinality', 'one'],
     ['telefone', 'cardinality', 'many']
 ];
 
-info.result = [
+App.data.result = [
   ['gabriel', 'endereço', 'av rio branco, 109', true],
   ['joão', 'endereço', 'rua bob, 88', true],
+  ["joão", "telefone", "234-5678", true],
   ['joão', 'telefone', '91234-5555', true],
   ['gabriel', 'telefone', '98888-1111', true],
   ['gabriel', 'telefone', '56789-1010', true]
 ];
 
-console.log(vigencia(info.facts, info.schema));
+App.main(App.data, App.filterFn);
